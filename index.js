220538caff5e5f9e4d597e97b6353120ffafd7bf
Vanessa <vanessa.co403@gmail.com>
2023-02-03 08:41:06 -0500

    fetch lab

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
   
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then (res => res.json())
+  .then (data => renderBooks(data));
+      
+ 
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
-  books.forEach(book => {
+  books.forEach(book =>{
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
